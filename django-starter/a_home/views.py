336fe6f9d9bdbb5460729ea52f1fca70e81bcf7b
Brian Michael <brianmichaeladero@gmail.com>
2025-10-13 21:15:02 +0300

    starting

--- django-starter/a_home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-
-
-def home_view(request):
-    context = {}
-    # return render(request, 'home.html', context)
-    return redirect("messageboard")
--- django-starter/a_messageboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("", messageboard_view, name="messageboard"),
-    path("subscribe/", subscribe, name="subscribe"),
-]
